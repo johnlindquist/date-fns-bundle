fb2ada621411a73fea3762f05499fad2a0ffe45f
John Lindquist <johnlindquist@gmail.com>
2025-03-12 17:04:38 -0600

    Initial commit: Setup date-fns custom bundle

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// Import the specific functions from date-fns
+import {
+    formatDistanceToNow,
+    compareAsc,
+    format,
+    parseISO,
+    formatDistanceToNowStrict
+} from 'date-fns';
+
+// Re-export them for the bundle
+export {
+    formatDistanceToNow,
+    compareAsc,
+    format,
+    parseISO,
+    formatDistanceToNowStrict
+}; 
